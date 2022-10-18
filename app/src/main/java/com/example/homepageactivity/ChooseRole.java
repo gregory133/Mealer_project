c6bd3e46cf0400209873de3fdf3bf5ff8c5384eb
gregory133 <jmarc134@uottawa.ca>
2022-10-17 20:03:50 -0400

    added more registration stuff

--- app/src/main/java/com/example/homepageactivity/ChooseRole.java
+++ app/src/main/java/com/example/homepageactivity/ChooseRole.java
@@ -2,7 +2,10 @@ package com.example.homepageactivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ChooseRole extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class ChooseRole extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_role);
+    }
+
+    public void onClickButton(View view){
+        Button button=(Button) view;
+        String tag=(String)button.getTag();
+
+        Intent intent=new Intent(this, RegistrationActivity.class);
+
+        intent.putExtra("TYPE", tag);
+
+        startActivity(intent);
+
     }
 }
--- app/src/main/java/com/example/homepageactivity/RegistrationActivity.java
+++ app/src/main/java/com/example/homepageactivity/RegistrationActivity.java
@@ -2,13 +2,69 @@ package com.example.homepageactivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class RegistrationActivity extends AppCompatActivity {
+
+    private TextView titleText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        titleText=findViewById(R.id.title);
+
+        Bundle extras=getIntent().getExtras();
+        String mode=extras.getString("TYPE");
+
+        String titleString=(String)titleText.getText();
+        titleText.setText(titleString+" "+mode);
+
     }
+
+    public void onClickRegisterButton(View view){
+        String firstName = (((EditText) findViewById(R.id.firstNameEdit)).getText()).toString();
+        String lastName = (((EditText) findViewById(R.id.lastNameEdit)).getText()).toString();
+        String email = (((EditText) findViewById(R.id.emailEdit)).getText()).toString();
+        String address = (((EditText) findViewById(R.id.addressEdit)).getText()).toString();
+        String password = (((EditText) findViewById(R.id.passwordEdit)).getText()).toString();
+        String confirmPassword = (((EditText) findViewById(R.id.confirmPasswordEdit)).getText()).toString();
+
+        if (validate(firstName, lastName, email, address, password, confirmPassword)){
+            Intent intent=new Intent(this, UserHomepageActivity.class);
+            startActivity(intent);
+        }
+        else{
+            Toast.makeText(this, "Invalid/Insufficient Information Given", Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+    /**
+     * rules for valid data entries given
+     * @param firstName
+     * @param lastName
+     * @param email
+     * @param address
+     * @param password
+     * @param confirmPassword
+     * @return
+     */
+    private boolean validate(String firstName, String lastName, String email, String address, String password, String confirmPassword){
+        if (firstName.equals("") || lastName.equals("") || email.equals("") || address.equals("") || password.equals("") || confirmPassword.equals("")){
+            return false;
+        }
+        if (!password.equals(confirmPassword)){
+            return false;
+        }
+        return true;
+    }
+
 }
