149e48023ae330b703e00da3df37ebb15f420d4b
Victor Matyiku <victormatiyku@gmail,com>
2022-10-19 16:30:46 -0400

    Basic registration functionality done

--- app/src/main/java/com/example/homepageactivity/RegistrationActivity.java
+++ app/src/main/java/com/example/homepageactivity/RegistrationActivity.java
@@ -1,18 +1,27 @@
 package com.example.homepageactivity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class RegistrationActivity extends AppCompatActivity {
 
     private TextView titleText;
+
+    private Button Btn;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,24 +36,20 @@ public class RegistrationActivity extends AppCompatActivity {
         String titleString=(String)titleText.getText();
         titleText.setText(titleString+" "+mode);
 
-    }
+        //firebase integration
 
-    public void onClickRegisterButton(View view){
-        String firstName = (((EditText) findViewById(R.id.firstNameEdit)).getText()).toString();
-        String lastName = (((EditText) findViewById(R.id.lastNameEdit)).getText()).toString();
-        String email = (((EditText) findViewById(R.id.emailEdit)).getText()).toString();
-        String address = (((EditText) findViewById(R.id.addressEdit)).getText()).toString();
-        String password = (((EditText) findViewById(R.id.passwordEdit)).getText()).toString();
-        String confirmPassword = (((EditText) findViewById(R.id.confirmPasswordEdit)).getText()).toString();
+        mAuth = FirebaseAuth.getInstance();
 
-        if (validate(firstName, lastName, email, address, password, confirmPassword)){
-            Intent intent=new Intent(this, UserHomepageActivity.class);
-            startActivity(intent);
-        }
-        else{
-            Toast.makeText(this, "Invalid/Insufficient Information Given", Toast.LENGTH_LONG).show();
-        }
+        Btn = findViewById(R.id.Register);
 
+        // Set on Click Listener on Registration button
+        Btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                registerNewUser();
+            }
+        });
     }
 
     /**
@@ -65,6 +70,57 @@ public class RegistrationActivity extends AppCompatActivity {
             return false;
         }
         return true;
+    }
+
+    private void registerNewUser() {
+
+        String firstName = (((EditText) findViewById(R.id.firstNameEdit)).getText()).toString();
+        String lastName = (((EditText) findViewById(R.id.lastNameEdit)).getText()).toString();
+        String email = (((EditText) findViewById(R.id.emailEdit)).getText()).toString();
+        String address = (((EditText) findViewById(R.id.addressEdit)).getText()).toString();
+        String password = (((EditText) findViewById(R.id.passwordEdit)).getText()).toString();
+        String confirmPassword = (((EditText) findViewById(R.id.confirmPasswordEdit)).getText()).toString();
+
+
+        // Validations for input email and password
+        if (!validate(firstName,lastName,email,address,password,confirmPassword)) {
+            Toast.makeText(this, "Invalid/Insufficient Information Given", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        // create new user or register new user
+        mAuth
+                .createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task)
+                    {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(getApplicationContext(),
+                                            "Registration successful!",
+                                            Toast.LENGTH_LONG)
+                                    .show();
+
+                            // if the user created intent to login activity
+                            Intent intent
+                                    = new Intent(RegistrationActivity.this,
+                                    MainActivity.class);
+                            startActivity(intent);
+                        }
+                        else {
+
+                            // Registration failed
+                            Toast.makeText(
+                                            getApplicationContext(),
+                                            "Registration failed!!"
+                                                    + " Please try again later",
+                                            Toast.LENGTH_LONG)
+                                    .show();
+
+                        }
+                    }
+                });
     }
 
 }
