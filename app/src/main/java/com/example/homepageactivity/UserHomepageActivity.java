35cbc80a7b4c1e19ddd2c3b184f09067c2c0d4a9
fellbeast9 <Wring8@gmail.com>
2022-10-20 19:35:13 -0400

    logout dropdown updated and outdated files deleted

--- app/src/main/java/com/example/homepageactivity/UserHomepageActivity.java
+++ app/src/main/java/com/example/homepageactivity/UserHomepageActivity.java
@@ -5,7 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class UserHomepageActivity extends AppCompatActivity {
 
@@ -14,8 +21,9 @@ public class UserHomepageActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_homepage);
 
-        System.out.println("at homepage");
-        //SetNotificationIconStatus(false);
+        setupPageSelectSpinner((Spinner) findViewById(R.id.pagesSpinner));
+
+
     }
 
     @Override
@@ -32,6 +40,43 @@ public class UserHomepageActivity extends AppCompatActivity {
         }
     }
 
+    public void setupPageSelectSpinner(Spinner pagesSpinner){
+        Class homepageClass = UserHomepageActivity.class;
+        final List<String> pageNames = Arrays.asList("Menu","Logout");
+        final List<Class> pageClasses = Arrays.asList(homepageClass, null);
+        //final Spinner spinner = findViewById(R.id.pagesSpinner);
+        ArrayAdapter adapter = new ArrayAdapter(getApplicationContext(),
+                android.R.layout.simple_spinner_item,
+                pageNames);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        pagesSpinner.setAdapter(adapter);
+
+        pagesSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                String pageSelectedName = pagesSpinner.getSelectedItem().toString();
+                if(pageClasses.get(i) != null){
+                    if(this.getClass().getName().contains(pageClasses.get(i).getName())) return;    //this.getClass() instanceof pageClasses.get(i)
+                    startActivityForResult(new Intent(getApplicationContext(), pageClasses.get(i)),0);
+                }else if(pageNames.get(i) == "Logout"){
+                    UserLogoutRequest();
+                }//1234@56.78
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+    }
+
+    public void UserLogoutRequest(){
+        Intent returnIntent = new Intent();
+        returnIntent.putExtra("Logout", 1);
+        setResult(RESULT_OK, returnIntent);
+        finish();
+    }
+
     private void HomepageLogout(){
         //Logout Operations///////////////////////////////////////////////////////
         finish();
@@ -41,7 +86,6 @@ public class UserHomepageActivity extends AppCompatActivity {
     public void onClickUserAccountButton(View view){
         Intent intent = new Intent(getApplicationContext(), UserAccountActivity.class);
         startActivityForResult(intent,0);
-        System.out.println("to account");
     }
 
 //    /**Opens UserInboxActivity*/
