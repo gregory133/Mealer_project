3d6d8540f4394a15fefb4d7900d78170213f7f4e
BrentMRPalmer <bpalm015@uottawa.ca>
2022-10-29 14:05:43 -0400

    Added active to complaint

--- app/src/main/java/com/example/homepageactivity/domain/ComplaintMessage.java
+++ app/src/main/java/com/example/homepageactivity/domain/ComplaintMessage.java
@@ -3,12 +3,14 @@ package com.example.homepageactivity.domain;
 public class ComplaintMessage extends Message {
     Cook cook; //the cook that received the complaint
     Admin admin; //the admin that selected the complaint
+    boolean active;
 
     public ComplaintMessage(String sender, String recipient, String subject, String body, Cook cook){
         super(sender, recipient, subject, body);
         this.cook = cook;
         //admin will be set once admin selects
         this.admin = null;
+        active = true;
     }
 
 
