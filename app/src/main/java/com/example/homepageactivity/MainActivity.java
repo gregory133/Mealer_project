7fb9e57574e5e63e7c96313e22bd52f50cc6ee37
BrentMRPalmer <bpalm015@uottawa.ca>
2022-11-17 20:26:57 -0500

    updated login

--- app/src/main/java/com/example/homepageactivity/MainActivity.java
+++ app/src/main/java/com/example/homepageactivity/MainActivity.java
@@ -2,21 +2,36 @@ package com.example.homepageactivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     private EditText emailTextView, passwordTextView;
     private FirebaseAuth mAuth;
+    FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -104,8 +119,58 @@ public class MainActivity extends AppCompatActivity {
         //Don't want to leave there info sitting around after logging in
         ClearLoginInfoBoxes();
         finish();
-        Intent intent = new Intent(getApplicationContext(), UserHomepageActivity.class);
-        startActivity(intent);
+
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        if (currentUser == null) {
+            Toast.makeText(this, "Error, no user signed in", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        db = FirebaseFirestore.getInstance();
+
+        DocumentReference docRef = db.collection("user").document(currentUser.getUid());
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        Map userData = document.getData(); //This return the data in the form of a Map or Dictionary by default (see next section about converting to a custom Java Object).
+                        openHomePage(userData);
+                        //what you want it to do with the data - remember, this is a new method so it can't directly talk to the method that created it and vice-versa
+                    } else {
+                        loginAttemptFailure();
+                        return;
+                    }
+                } else {
+                    loginAttemptFailure();
+                    return;
+                }
+            }
+        });
+
+
+    }
+
+    private void openHomePage(Map userData){
+        Intent intent;
+        switch ((String) userData.get("role")){
+            case "Cook":
+                intent = new Intent(getApplicationContext(), CookLandingPage.class);
+                startActivity(intent);
+            case "Admin":
+                intent = new Intent(getApplicationContext(), InboxActivity.class);
+                startActivity(intent);
+            case "Client":
+                intent = new Intent(getApplicationContext(), UserHomepageActivity.class);
+                startActivity(intent);
+            default:
+                loginAttemptFailure();
+                return;
+        }
+
+
     }
 
 
--- app/src/main/java/com/example/homepageactivity/domain/Meal.java
+++ app/src/main/java/com/example/homepageactivity/domain/Meal.java
@@ -4,12 +4,37 @@ import android.media.Image;
 
 public class Meal {
 
-    Image picture;
-    String name;
-    String description;
-    String cuisineType;
-    String mealType;
-    String[] allergens;
-    double price;
+    //Image picture;
+    private String mealName;
+    private String mealType;
+    private String cuisineType;
+    private String[] ingredients;
+    private String[] allergens;
+    private double price;
+    private String description;
+    private String cookUID;
+
+    public Meal(){};
+
+    public Meal(String mealName, String mealType, String cuisineType, String[] ingredients,
+                String[] allergens, double price, String description, String cookUID){
+        this.mealName = mealName;
+        this.mealType = mealType;
+        this.cuisineType = cuisineType;
+        this.ingredients = ingredients;
+        this.allergens = allergens;
+        this.price = price;
+        this.description = description;
+        this.cookUID = cookUID;
+    }
+
+    public String getMealName(){ return mealName; }
+    public String getMealType(){ return mealType; }
+    public String getCuisineType() { return cuisineType; }
+    public String[] getIngredients() { return ingredients; }
+    public String[] getAllergens() {return allergens; }
+    public double getPrice() { return price; }
+    public String getDescription() { return description; }
+    public String getCookUID() { return cookUID; }
 
 }
